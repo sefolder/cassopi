a24305e09c26a6cef18427590a2f8af46e39baa4
ybuser <ybuser@naver.com>
2022-03-05 23:51:37 +0900

    ga added

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,13 +3,33 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { Provider } from "react-redux";
 import { store } from "../settings/store";
+import { useEffect } from "react";
+import { useRouter } from 'next/router';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </Provider>
-);
+import * as ga from '../lib/ga';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events])
+
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
+  
+}
 
 export default MyApp;
--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -8,6 +8,8 @@ import Document, {
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
+// process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
@@ -48,6 +50,22 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
           />
         </Head>
         <body>
